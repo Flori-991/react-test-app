c1a0c35b3113dcc2b7cc4239aafc9734b1b31baa
Flori-991 <Florian.Hecek@red-gate.com>
2023-06-15 10:19:13 +0100

    Set up open API use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import axios, {Axios} from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const placeHolder = 'Search for word';
+    const [wordToSearch, setWordToSearch] = useState('');
+    const [dictEntry, setDictEntry] = useState('');
+    const [apiResponse, setApiResponse] = useState(null);
+
+    const getDictEntry = async () => {
+        //setApiResponse(await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordToSearch));
+
+        axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordToSearch)
+            .catch(function (error) {setApiResponse(null)})
+            .then(res => setApiResponse(res));
+
+        console.log(apiResponse);
+
+        if (apiResponse !== null && apiResponse !== undefined ){
+            setDictEntry(apiResponse.data[0].phonetic);
+        }
+        else {
+            setDictEntry('This is not in our database, sorry!')
+        }
+    }
+
+    return (
+        <div className="App">
+            <input placeholder={placeHolder} id="wordToSearch" onChange={event => setWordToSearch(event.target.value)}/>
+            <button onClick={getDictEntry}>Search</button>
+            <h1>{dictEntry}</h1>
+        </div>
+    );
 }
 
 export default App;
